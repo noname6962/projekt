f22e31ddd856ee6d520e94eaf53a325507e25e9c
Adam <127439394+noname6962@users.noreply.github.com>
2025-01-09 14:16:53 +0100

    Delete python/application.py

--- python/application.py
+++
@@ -1,6 +0,0 @@
-from back import app as application  #  Import your Flask app instance from back.py
-
-
-#  Optional: If you need a specific port, configure it here
-if __name__ == "__main__":
-    application.run(debug=False, port=8080)  # Set port if needed for AWS
