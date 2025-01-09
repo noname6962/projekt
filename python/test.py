ef23d9f46189756977a02f6642b96283fcc4e33f
Adam <127439394+noname6962@users.noreply.github.com>
2025-01-09 14:17:18 +0100

    Delete python/test.py

--- python/test.py
+++
@@ -1,7 +0,0 @@
-# Assuming `result` is the string "6c1fa6624d32ce4066648e97b0ce8b99:ghpsnanakp"
-result = "6c1fa6624d32ce4066648e97b0ce8b99:ghpsnanakp"
-
-# Split the string at the colon and take the second part
-cut_result = result.split(':', 1)[1]
-
-print(cut_result)  # Output: ghpsnanakp
